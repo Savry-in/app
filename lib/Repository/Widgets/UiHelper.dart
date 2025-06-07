b182f235716a053b434fca5d566926c16ce147c1
Tarun Rathi <tarunrat12@gmail.com>
2025-06-07 15:24:17 +0530

    topnavbar widget

--- lib/Repository/Widgets/UiHelper.dart
+++ lib/Repository/Widgets/UiHelper.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/cupertino.dart';
 
-class UiHelper{
-  static CustomImage({required String img}){
+class UiHelper {
+  static CustomImage({required String img}) {
     return Image.asset("assets/Images/$img");
   }
 
+  static CustomText({
+    required String text,
+    required Color color,
+    required FontWeight fontWeight,
+    String? FontFamily,
+    required double fontSize,
+  }) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontSize,
+        fontFamily: FontFamily ?? "regular",
+        fontWeight: fontWeight,
+        color: color,
+      ),
+    );
+  }
 }
