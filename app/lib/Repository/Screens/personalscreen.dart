9404f6a9863d0023e3eb35486113ab980c897960
Tarun Rathi <tarunrat12@gmail.com>
2025-07-02 12:41:30 +0530

    commit for welcomeScreen and personalScreen

--- app/lib/Repository/Screens/personalscreen.dart
+++ app/lib/Repository/Screens/personalscreen.dart
@@ -5,9 +5,10 @@ import 'package:savry_app_development/Repository/Widgets/progressbar.dart';
 import 'package:savry_app_development/Repository/Widgets/uihelper.dart';
 
 class 	FamilyPreferenceScreen extends StatefulWidget {
-  // ignore: library_private_types_in_public_api
+
   const FamilyPreferenceScreen({super.key});
 
+  // ignore: library_private_types_in_public_api
   @override
   _FamilyPreferenceScreenState createState() => _FamilyPreferenceScreenState();
 }
